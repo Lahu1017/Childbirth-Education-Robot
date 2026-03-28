e079b8e5d4a92ae2272bee654864ff293def0b95
Lahu1017 <qqq0963115346@gmail.com>
2026-03-28 23:27:34 +0800

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    // 忽略 TypeScript 檢查
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // 忽略 ESLint 檢查
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
